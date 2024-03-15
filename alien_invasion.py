c96e656e7ff3d764637b9b5708539b98eb45ec3a
Leandro Pereira <leandro.gp@gmail.com>
2024-03-14 22:34:04 -0300

    Ship moving to the Left and Right

--- alien_invasion.py
+++ alien_invasion.py
@@ -44,9 +44,14 @@ class AlienInvasion:
                 if event.key == pygame.K_RIGHT:
                     #Move the ship to the right one pixel a time
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    #Move the ship to the right one pixel a time
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
     
     def _update_screen(self):
         '''Update images on the screen, and flip to the new screen.'''
