64d9a08692b1073c88dad859c7e331fd03ecf029
lpereir <119885265+lpereir@users.noreply.github.com>
2024-10-02 21:16:00 -0300

    Chapter 15 - Make a histogram

--- Chapter-15-GeneratingData/die_visual.py
+++ Chapter-15-GeneratingData/die_visual.py
@@ -1,3 +1,4 @@
+import plotly.express as px
 from die import Die
 #Create a D6
 die = Die()
@@ -14,5 +15,9 @@ poss_results = range(1,die.num_sides+1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
+
+#Visualize the results.
+fig = px.bar(x=poss_results, y=frequencies)
+fig.show()
 
 print(frequencies)
